3a066b85880ff8478db38fb952fc6af7064de154
ParthSali <parthsali04@gmail.com>
2024-09-02 17:35:41 +0530

    Add Loading Screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import { Canvas } from "@react-three/fiber";
 import { Avatar } from "./components/Avatar";
 import Scene from "./components/Scene";
 import { Leva } from "leva";
+import { Suspense, useState } from "react";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 const App = () => {
+  const [start, setStart] = useState(false);
+
   return (
-    <Canvas
-      camera={{
-        position: [0, 0.5, 6],
-        fov: 50,
-      }}
-    >
-      <Scene />
-    </Canvas>
+    <>
+      <Canvas
+        camera={{
+          position: [0, 0.5, 6],
+          fov: 50,
+        }}
+      >
+        <Suspense fallback={null}>{start && <Scene />}</Suspense>
+      </Canvas>
+      <LoadingScreen started={start} onStarted={() => setStart(true)} />
+    </>
   );
 };
 
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,30 @@
+import { useProgress } from "@react-three/drei";
+
+export const LoadingScreen = ({ started, onStarted }) => {
+  const { progress } = useProgress();
+  return (
+    <div className={`loadingScreen ${started ? "loadingScreen--started" : ""}`}>
+      <div className="loadingScreen__background" />
+      <div className="loadingScreen__board">
+        <h1 className="loadingScreen__title">
+          Loading {Math.round(progress)}%
+        </h1>
+        <button
+          className="loadingScreen__button"
+          disabled={progress < 100}
+          onClick={onStarted}
+        >
+          Start
+        </button>
+      </div>
+      <div className="loadingScreen__progress">
+        <div
+          className="loadingScreen__progress__value"
+          style={{
+            width: `${progress}%`,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
